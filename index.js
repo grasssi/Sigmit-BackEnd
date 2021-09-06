2f7e15121587171c39a6035d4ce1693aea3849d8
Amine Grassi <grassi.amine@hotmail.fr>
2021-09-06 23:50:59 +0100

    Add remote

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//get all users
+//get one user
+//get user by id
+//update user by id
+//delete user by id 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
