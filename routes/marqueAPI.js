186b94ef399fe52ff9d4019e313cfe9547be65e9
Amine Grassi <grassi.amine@hotmail.fr>
2021-10-26 15:43:50 +0200

    marque Api and controllers

---
+++ routes/marqueAPI.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+// reuire Controller
+const marqueController = require('../controllers/marqueController')
+
+// get all user
+router.get('/allmarques', marqueController.allMarques)
+
+// add one user
+router.post('/addmarque', marqueController.addMarque)
+
+//getone user by id
+router.get('/getmarque/:id', marqueController.getMarque)
+
+// update user by id
+router.put('/updatemarque/:id', marqueController.updateMarque)
+
+// delete user by id
+router.delete('/removemarque/:id', marqueController.removeMarque)
+
+module.exports = router;
